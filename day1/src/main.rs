6081b48062c5e6bc4a4d73978711b32b44a397e7
sinkingpoint <colin@quirl.co.nz>
2021-12-01 16:09:21 +1100

    Add day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,10 @@
+use libadvent::*;
+
 fn main() {
-    println!("Hello, world!");
+    let input = must_parse_to_ints(must_read_input_to_lines());
+    let count = (1..input.len()).filter(|&i| input[i] > input[i-1]).count();
+    let count2 = (1..input.len()-2).filter(|&i| input[i] + input[i+1] + input[i+2] > input[i-1] + input[i+1] + input[i]).count();
+    
+    println!("{}", count);
+    println!("{}", count2);
 }
